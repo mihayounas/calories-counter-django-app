5fa780d28428400ff8a5a21e5d57c00c661e2818
mihayounas <76411576+mihayounas@users.noreply.github.com>
2023-06-25 13:57:43 +0000

    landing page style

--- eatsmart/settings.py
+++ eatsmart/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import os
+
 from pathlib import Path
 from env import SECRET_KEY
 
@@ -122,9 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'counter', 'templates', 'images'),
-]
 
 
 # Default primary key field type
